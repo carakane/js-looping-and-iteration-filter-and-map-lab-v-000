8ca4895064b383c1681fb345b88c75fc2c4b7f21
Cara Kane <cara.kane@gmail.com>
2017-11-09 18:50:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function driversWithRevenueOver(drivers, revenue) {
 function driverNamesWithRevenueOver(drivers, revenue) {
   let x = drivers.filter(driver => driver.revenue > revenue)
   console.log(x)
-  // return x.filter(driver => driver.name)
+  let y = x.filter(driver => driver.name)
+  console.log(y)
 }
 
 function exactMatch() {
