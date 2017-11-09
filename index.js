9ea6c247ce07d5061e8a41837673135cdeb9dfa4
Cara Kane <cara.kane@gmail.com>
2017-11-09 19:00:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ function driversWithRevenueOver(drivers, revenue) {
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  return drivers.filter(driver => driver.revenue > revenue).map(driver => driver.name)
-}
-function driverNamesWithRevenueOver(drivers, revenue) {
   const x = drivers.filter(driver => driver.revenue > revenue)
   return x.map(driver => driver.name)
 }
