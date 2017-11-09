08ba3a82b319233616d4510a81516495d6fbe59a
Cara Kane <cara.kane@gmail.com>
2017-11-09 18:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function driversWithRevenueOver(drivers, revenue) {
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  return drivers.filter(driver => driver.revenue > revenue)
+  return drivers.filter(driver.name => driver.revenue > revenue)
 }
 
 function exactMatch() {
